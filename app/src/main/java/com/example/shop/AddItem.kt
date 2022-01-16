7a839d24f686cd6df30440e0aca1fdc4b3eee5ab
Milena <milenamazur00@gmail.com>
2022-01-16 16:12:19 +0100

    adding new items second approach added

--- app/src/main/java/com/example/shop/AddItem.kt
+++ app/src/main/java/com/example/shop/AddItem.kt
@@ -1,15 +1,15 @@
 package com.example.shop
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import android.net.Uri
-
+import android.os.Bundle
+import android.view.View
+import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.shop.adapter.ProductAdapter
 import com.example.shop.dataSource.ProductDataSource
+import com.example.shop.model.Product
 import kotlinx.android.synthetic.main.activity_add_item.*
 
 
@@ -26,8 +26,19 @@ class AddItem : AppCompatActivity() {
 
         val addPhoto = findViewById<Button>(R.id.addPhoto)
 
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.categories,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
         addPhoto.setOnClickListener {
             getContent.launch("image/*")
+            oldImage.visibility = View.GONE
         }
 
         val add = findViewById<Button>(R.id.add)
@@ -42,11 +53,24 @@ class AddItem : AppCompatActivity() {
         val price = cena.text.toString()
         val color = kolorNowego.text.toString()
         val desc = opis.text.toString()
+        val categoryId = when (spinner.selectedItem.toString()) {
+            getString(R.string.dla_mezczyzn) -> R.string.dla_mezczyzn
+            getString(R.string.krem_na_noc) -> R.string.krem_na_noc
+            getString(R.string.krem_na_dzien) -> R.string.krem_na_dzien
+            getString(R.string.zel_do_mycia) -> R.string.zel_do_mycia
+            else -> {
+                0
+            }
+        }
 
-        ProductDataSource().addItem()
-        ProductDetailsActivity().addItem(name, capacity, price, color, desc)
-        ProductAdapter(this, ProductDataSource().loadProductsMain()).addItem(name, capacity, price, color)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        if (name == "" || capacity == "" || price == "" || color == "" || desc == "" || categoryId == 0) {
+            val toast = Toast.makeText(this, "Musisz wypelnić wszystkie pola!", Toast.LENGTH_LONG)
+            toast.show()
+        } else {
+            val newProduct = Product(name, capacity + "ml", desc, price + "zł", categoryId, "#$color", R.drawable.image23, false, 0)
+            ProductDataSource().addProduct(newProduct)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
